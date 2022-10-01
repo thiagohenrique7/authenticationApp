48ac3ff85dcc2bd0f993ed1b14ac58c712ab6523
henriquewallace <henrique.wallace@townsq.com.br>
2022-10-01 03:41:34 -0300

    AUTH - Delete app component template

--- authentication-app/src/app/app.component.ts
+++ authentication-app/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 import { TranslateService } from '@ngx-translate/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
